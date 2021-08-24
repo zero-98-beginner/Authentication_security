1849d84aef397dfeb084b0310d7a555ac78e6c0d
Sk halder <soumyacfc@gmail.com>
2021-08-24 16:53:27 +0530

    Added Hashing

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 
 const ejs = require("ejs");
 const mongoose= require("mongoose");
-const encrypt= require("mongoose-encryption");
+const md5=require("md5");
+
 
 const app = express();
 console.log(process.env.SECRET);
@@ -24,7 +25,7 @@ mongoose.connect("mongodb://localhost:27017/userDB" ,{useNewUrlParser:true ,useU
  });
 
  
- userSchema.plugin(encrypt ,{secret:process.env.SECRET ,encryptedFields:["password"] });
+
  const User =new mongoose.model("User" , userSchema);
 
 app.get("/",function(req, res){
@@ -42,7 +43,7 @@ app.get("/login",function(req, res){
 app.post("/register" ,function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password:req.body.password
+    password: md5(req.body.password)
 
   });
   newUser.save(function(err){
@@ -55,7 +56,7 @@ app.post("/register" ,function(req,res){
 });
 app.post("/login" ,function(req,res){
   const username =req.body.username;
-  const password =req.body.password;
+  const password =md5(req.body.password);
 
 User.findOne({email:username} ,function(err,foundUser){
   if(err){
